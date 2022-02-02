5003858f56085cebf7d4fdce3ecc36f65d8c5dcd
Jesper de Jong <jespdj@gmail.com>
2022-02-02 22:07:25 +0100

    Rename Customer1, 2 to Customer5, 6 to avoid confusion.

--- src/main/java/com/jesperdj/java_level_17/f_records_2/Customer5.java
+++ src/main/java/com/jesperdj/java_level_17/f_records_2/Customer5.java
@@ -3,5 +3,5 @@ package com.jesperdj.java_level_17.f_records_2;
 // Take a look at this "stringly typed" record (that's a pun on "strongly typed").
 // You could put anything in the 'emailAddress' and 'phoneNumber' fields. You could add a constructor with validation,
 // but still the type String of these fields doesn't have any meaning in the business domain.
-public record Customer1(String name, String emailAddress, String phoneNumber) {
+public record Customer5(String name, String emailAddress, String phoneNumber) {
 }
--- src/main/java/com/jesperdj/java_level_17/f_records_2/Customer6.java
+++ src/main/java/com/jesperdj/java_level_17/f_records_2/Customer6.java
@@ -3,5 +3,5 @@ package com.jesperdj.java_level_17.f_records_2;
 // To improve this, you can create domain-specific types: EmailAddress and PhoneNumber.
 // Then you can implement the validation for those specific types there, and it can easily be reused.
 // Records make creating domain-specific types easy and worthwhile because it only takes a few lines to define a record.
-public record Customer2(String name, EmailAddress emailAddress, PhoneNumber phoneNumber) {
+public record Customer6(String name, EmailAddress emailAddress, PhoneNumber phoneNumber) {
 }
