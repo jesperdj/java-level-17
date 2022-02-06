938785586e6213f501a4dfd8d264e540811da421
Jesper de Jong <jespdj@gmail.com>
2022-02-06 20:30:28 +0100

    Simplify ExhaustivenessCheck.java

--- src/main/java/com/jesperdj/java_level_17/b_switch/ExhaustivenessCheck.java
+++ src/main/java/com/jesperdj/java_level_17/b_switch/ExhaustivenessCheck.java
@@ -18,14 +18,6 @@ public class ExhaustivenessCheck {
 
         // An advantage of switch expressions over switch statements is that the compiler will check that there is a case that covers every possible
         // input value. This is necessary because an expression must always result in a value.
-        // Here we are required to add a default case (even though this will never be evaluated in practice).
-        int value = random.nextInt(1, 11);
-        var message1 = switch (value) {
-            case 1, 2, 3, 4, 5 -> "Countable on one hand";
-            case 6, 7, 8, 9, 10 -> "Countable on two hands";
-            default -> throw new IllegalStateException("Unexpected value");
-        };
-
         // This is especially nice with enums, because it protects you from a bug when you forget to add a case for an enum constant (as long as you don't
         // have a default case). Also, if you add a constant to the enum later, the switch expression will not compile anymore.
         Status status = Status.values()[random.nextInt(Status.values().length)];
